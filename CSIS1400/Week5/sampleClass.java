deed0665814990ea67f5df1d4529255334fc417d
Chad <chad.zuniga10@gmail.com>
2018-09-18 17:13:08 -0600

    added notes for week 5

---
+++ CSIS1400/Week5/sampleClass.java
@@ -0,0 +1,17 @@
+/***************************************************
+* Author: Chad Zuniga
+* 
+* Monogram that consists of the initials
+* of the first name, middle name, and last name
+* and also specifies a color that is limited to the 
+* choices black, silver, or gold
+*****************************************************/
+
+public class Sample{
+   // fileds
+   
+   // constructors
+   
+   // methods
+
+}
---
+++ CSIS1400/Week5/sampleClassApp.java
@@ -0,0 +1,12 @@
+/***************************
+* Author: Chad Zuniga
+* 
+* Test client for Monogram
+
+***************************/
+
+public class SampleApp {
+   public static void main(String[] args) {
+   
+   }
+}
